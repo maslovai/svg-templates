19e9102884db0b87277f5103bfcdeb5d7f0f8384
Iryna Maslova <maslovai@hotmail.com>
2020-07-02 10:25:41 -0700

    remove extra code

--- src/components/left-bottom/left-bottom.js
+++ src/components/left-bottom/left-bottom.js
@@ -16,9 +16,6 @@ export default function LeftBottom(props){
             </Tabs> 
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="6 9 12 15 18 9"></polyline></svg>
             </div>
-            <div className = {visible? "showPane ":"hidePane w3-animate-bottom"}> 
-                <h1>{props.header}</h1>
-            </div>
             
         </div>
     )
