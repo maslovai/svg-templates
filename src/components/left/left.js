443aca06392df8d0876734a2884ca8509ae4ada6
Iryna Maslova <maslovai@hotmail.com>
2020-07-03 11:05:16 -0700

    resizing paned added, components refactored

--- src/components/left/left.js
+++ src/components/left/left.js
@@ -3,14 +3,17 @@ import SplitPane from 'react-split-pane';
 import { Tabs} from '@tableau/tableau-ui';
 import LeftTopTabOne from'./tab1';
 import LeftTopTabTwo from './tab2';
-import LeftBottom from '../left-bottom/left-bottom';
 
 export default function Left(){
     const [selectedTabIndex,setIndex]=useState(0);
-    const  [tabs] = useState([{content:<LeftTopTabOne/>,title:"tab1"},
+    const  [tabs1] = useState([{content:<LeftTopTabOne/>,title:"tab1"},
                    {content:<LeftTopTabTwo/>,title:"tab2"}]);
+    const [tabs2] = useState([{content:<div><h1>SOME DIV</h1></div>,title:"left-bottom"}]);
+    const [visible, toggleVisible] = useState(true);
+    const [clicked, setClicked] = useState([<polyline points="18 15 12 9 6 15"></polyline>,
+                                            <polyline points="6 9 12 15 18 9"></polyline>]);
     return (
-        <SplitPane split="horizontal" minSize={100} defaultSize={500}>
+        <SplitPane split="horizontal" minSize={100} defaultSize={visible?500:650}>
             <div> 
                 <Tabs 
                     onTabChange={(index) => {
@@ -18,14 +21,22 @@ export default function Left(){
                         setIndex(index);
                         }}
                     selectedTabIndex={selectedTabIndex}
-                    tabs={tabs}
+                    tabs={tabs1}
                 >
-                    {tabs[selectedTabIndex].content}
-                </Tabs>
-                
+                    {tabs1[selectedTabIndex].content}
+                </Tabs>    
             </div >
-
-            <LeftBottom header='LEFT_BOTTOM'/>
+            <div className = {visible ? "topPane" : "topPane"}> 
+                <Tabs
+                    tabs={tabs2}
+                >
+                    {tabs2[0].content}
+                </Tabs> 
+                <svg  className ="icon" onClick={()=>toggleVisible(!visible)}
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            {visible?clicked[1]:clicked[0]} 
+                </svg>
+        </div>  
         </SplitPane>
     )
 }
--- src/components/right/right.js
+++ src/components/right/right.js
@@ -3,15 +3,18 @@ import SplitPane from 'react-split-pane';
 import {Tabs} from '@tableau/tableau-ui';
 import RightTopTabOne from'./tab1';
 import RightTopTabTwo from './tab2';
-import RightBottom from '../right-bottom/right-bottom';
-
+import Errors from '../right-bottom/errors';
 
 export default function Right(){
     const [selectedTabIndex,setIndex]=useState(0);
     const  [tabs] = useState([{content:<RightTopTabOne/>,title:"tab1"},
                    {content:<RightTopTabTwo/>,title:"tab2"}]);
+    const [visible, toggleVisible] = useState(true);
+    const  [tabs2] = useState([{content:<Errors/>,title:"errors"}]);
+    const [clicked, setClicked] = useState([<polyline points="18 15 12 9 6 15"></polyline>,
+                                            <polyline points="6 9 12 15 18 9"></polyline>]);
     return (
-        <SplitPane split="horizontal" minSize={100} defaultSize={500}>
+        <SplitPane split="horizontal" minSize={100} defaultSize={visible?500:650}>
             <div> 
                 <Tabs 
                     onTabChange={(index) => {
@@ -24,7 +27,17 @@ export default function Right(){
                     {tabs[selectedTabIndex].content}
                 </Tabs>  
             </div>
-            <RightBottom header='RIGHT_BOTTOM' />
+            <div className = {visible ? "topPane" : "topPane"}> 
+                <Tabs
+                    tabs={tabs2}
+                >
+                    {tabs2[0].content}
+                </Tabs> 
+                <svg  className ="icon" onClick={()=>toggleVisible(!visible)}
+                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    {visible?clicked[1]:clicked[0]} 
+                </svg>
+            </div>   
         </SplitPane>
     )
 }
