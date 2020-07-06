5f899e57288accc9e335d26cda3153f156ee424a
Iryna Maslova <maslovai@hotmail.com>
2020-07-06 12:32:44 -0700

    commiting before adding redux

--- src/components/left/left.js
+++ src/components/left/left.js
@@ -8,12 +8,12 @@ export default function Left(){
     const [selectedTabIndex,setIndex]=useState(0);
     const  [tabs1] = useState([{content:<LeftTopTabOne/>,title:"tab1"},
                    {content:<LeftTopTabTwo/>,title:"tab2"}]);
-    const [tabs2] = useState([{content:<div><h1>SOME DIV</h1></div>,title:"left-bottom"}]);
+    const [tabs2] = useState([{content:<div><h1>Some div</h1></div>,title:"left-bottom"}]);
     const [visible, toggleVisible] = useState(true);
-    const [clicked, setClicked] = useState([<polyline points="18 15 12 9 6 15"></polyline>,
+    const [clicked] = useState([<polyline points="18 15 12 9 6 15"></polyline>,
                                             <polyline points="6 9 12 15 18 9"></polyline>]);
     return (
-        <SplitPane split="horizontal" minSize={100} defaultSize={visible?500:650}>
+        <SplitPane split="horizontal" minSize={100} defaultSize={visible?500:700}>
             <div> 
                 <Tabs 
                     onTabChange={(index) => {
@@ -26,14 +26,14 @@ export default function Left(){
                     {tabs1[selectedTabIndex].content}
                 </Tabs>    
             </div >
-            <div className = {visible ? "topPane" : "topPane"}> 
+            <div className = "topPane"> 
                 <Tabs
                     tabs={tabs2}
                 >
                     {tabs2[0].content}
                 </Tabs> 
                 <svg  className ="icon" onClick={()=>toggleVisible(!visible)}
-                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                             {visible?clicked[1]:clicked[0]} 
                 </svg>
         </div>  
--- src/components/right/right.js
+++ src/components/right/right.js
@@ -14,7 +14,7 @@ export default function Right(){
     const [clicked, setClicked] = useState([<polyline points="18 15 12 9 6 15"></polyline>,
                                             <polyline points="6 9 12 15 18 9"></polyline>]);
     return (
-        <SplitPane split="horizontal" minSize={100} defaultSize={visible?500:650}>
+        <SplitPane split="horizontal" minSize={100} defaultSize={visible?500:700}>
             <div> 
                 <Tabs 
                     onTabChange={(index) => {
@@ -34,7 +34,7 @@ export default function Right(){
                     {tabs2[0].content}
                 </Tabs> 
                 <svg  className ="icon" onClick={()=>toggleVisible(!visible)}
-                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                     {visible?clicked[1]:clicked[0]} 
                 </svg>
             </div>   
